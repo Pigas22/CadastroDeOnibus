c0d6281f38457f15188d35b62d757efad1eb45ae
Pigas22 <125706324+Pigas22@users.noreply.github.com>
2023-10-24 18:54:50 +0000

    arquivos do replit

---
+++ venv/lib/python3.10/site-packages/interface/__init__.py
@@ -0,0 +1,9 @@
+from .default import default
+from .interface import implements, Interface, InvalidImplementation
+
+__all__ = [
+    "default",
+    "InvalidImplementation",
+    "Interface",
+    "implements",
+]
